43ef59dcadf82e876d55f9246cd6e73d3f2fa778
Wayne <wayne.5540@gmail.com>
2017-09-05 19:46:32 +0800

    add body parser and kcors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import Koa from 'koa';
 import api from './api';
+import bodyParser from 'koa-bodyparser';
+import cors from 'kcors';
+
+
 import Poloniex from 'poloniex.js';
 
 const poloniex = new Poloniex();
 
 const app = new Koa()
+  .use(cors())
+  .use(bodyParser())
   .use(async (ctx, next) => {
     ctx.body = 'Hello Koa';
     await next();
